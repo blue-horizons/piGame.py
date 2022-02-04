3bba2229d6ce43b145923104903364d3ff74b209
blue-horizons <jojotraingeek@gmail.com>
2022-02-04 08:48:17 +0000

    edits

--- pi.py
+++ pi.py
@@ -1,37 +1,55 @@
 import simple_chalk
-pi_large = open("pi.txt", "r")
+from os import name, system
 
-pi = ""
-colour_pi = ""
+
+
+
+gameMode = True
+pi_large = open("pi.txt", "r") #import large pi
+
+pi = "" #input
+colour_pi = "" #set output
 
 print("""This is a simple game.
 Type in as many digits of pi as possible. 
 Once you've finished, press enter.
 
 The game will output your results colour coded red if it's
-wrong, green if it's correct, red if not.
+wrong, green if it's correct.
 
 ENJOY!!!
 """)
-
-pi = input("Type your PI!!!\n> ")
-
-for x in range(0, len(pi)):
+while gameMode:
+    pi = input("Type your PI!!!\n> ")
     if str(pi[1]) != ".":
         print("Psst! Pi is a decimal number")
     if str(pi) == "π":
         print("smart alec. Do it properly XD")
-    if pi[x] == pi_large[x]:
-        colour_pi += chalk.green(x)
-    else:
-        colour_pi += chalk.red(x)
-    x += 1
 
-print("Your PI")
-print(pi)
-print("Correct PI")
-print(pi_large[0,len(pi)])
+    for x in range(0, len(pi)):
+        if pi[x] == pi_large[x]:
+            colour_pi += chalk.green(x)
+        else:
+            colour_pi += chalk.red(x)
+        x += 1
 
+    print("Your PI")
+    print(pi)
+    print("Correct PI")
+    print(chalk.blue(pi_large[0,len(pi)]))
+    carryOn = input("""Carry on?
+    y/N
+    > """)
+    if carryOn.lower() == "y":
+        if name == "nt":
+            _ = system("cls")
+        else:
+            _ = system("clear")
+
+
+
+
+"""
 print("MASSIVE PI MWAHAHAHAHAHA!!!!!!")
 print(pi_large)
-print("P.S. Sorry if your computer broke <3 :)")
+print("P.S. Sorry if your computer broke <3 :)")"""
